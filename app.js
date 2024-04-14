e9249e587eb0fbb6bdcc50aa38fe65dccf7c7671
Mohamed Ayman <mohikel6@gmail.com>
2024-04-14 14:55:52 +0200

    added colors

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ const divs = Array.from(document.querySelectorAll("div"));
 const data = document.querySelector(".data");
 
 const calculateAge = () => {
-  past = new Date(date.value);
-  let today = new Date();
-  let diff = Math.floor(today.getTime() - past.getTime());
-  let day = 1000 * 60 * 60 * 24;
+  if (date.value != "") {
+    past = new Date(date.value);
+    let today = new Date();
+    let diff = Math.floor(today.getTime() - past.getTime());
+    let day = 1000 * 60 * 60 * 24;
 
-  let numberOfDays = Math.floor(diff / day);
-  let months = Math.floor((numberOfDays % 365) / 30);
-  let years = Math.floor(numberOfDays / 365);
-  let days = Math.floor((numberOfDays % 365) % 30);
+    let numberOfDays = Math.floor(diff / day);
+    let months = Math.floor((numberOfDays % 365) / 30);
+    let years = Math.floor(numberOfDays / 365);
+    let days = Math.floor((numberOfDays % 365) % 30);
 
-  divs[0].firstElementChild.textContent = years;
-  divs[1].firstElementChild.textContent = months;
-  divs[2].firstElementChild.textContent = days;
+    divs[0].firstElementChild.textContent = years;
+    divs[1].firstElementChild.textContent = months;
+    divs[2].firstElementChild.textContent = days;
 
-  data.style.visibility = "visible";
+    data.style.visibility = "visible";
 
-  console.log(`${years}, ${months}, ${days}`);
+    console.log(`${years}, ${months}, ${days}`);
+  }
 };
